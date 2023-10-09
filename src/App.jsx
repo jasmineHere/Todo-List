166fad226652f51ef3ca17bc07161cd52cff1abb
Jasmine Muman <jasminemuman21@gmail.com>
2023-10-09 17:03:33 -0400

    added clear all tasks feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,71 @@
 import { useState } from "react";
-import './App.css';
+import "./App.css";
 
 function App() {
-  const [task, setTask] = useState(''); //declare a state variable for tasks
-  const [taskList, setTaskList] = useState([]); // declare a state variable for lists
-  
-  const handleClick = () => {
-    if(task.trim() !== '') { // make sure empty tasks are not added
-      setTaskList(prev => [...prev, task]);
-      setTask(''); // clear the task input
-    }
-  }; 
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+  const [completedTasks, setCompletedTasks] = useState([]);
 
-  const handleTaskCompletion = (indexToRemove) => {
-    setTaskList(prev => prev.filter((_, index) => index !== indexToRemove));
+  const handleClick = () => {
+    if (task.trim() !== "") {
+      setTaskList((prev) => [...prev, task]);
+      setCompletedTasks((prev) => [...prev, false]);
+      setTask("");
+    }
+  };
+
+  const clearAllTasks = () => {
+    setTaskList([]);
+    setCompletedTasks([]);
+  };
+
+  const handleTaskCompletion = (indexToComplete) => {
+    setCompletedTasks((prev) =>
+      prev.map((completed, index) => {
+        if (index === indexToComplete) {
+          return !completed;
+        }
+        return completed;
+      })
+    );
   };
 
   return (
     <div className="App">
-        <h1>
-          My T0-DO-LIST
-        </h1>
-        <div className='container'>
-          <label>
-            <b>Enter your task here: </b> 
-            < input value={task} 
-              name="myInput" 
-              defaultValue="Example Task"
-              onChange={e => setTask(e.target.value)}/>
-          </label>
-          <button onClick={() => handleClick()}>Add</button>
-        {/* {taskList&& taskList.map((task) => <p>{task}</p>)} */}
+      <h1>My T0-DO-LIST</h1>
+      <div className="container">
+        <label>
+          <b>Enter your task here: </b>
+          <input
+            value={task}
+            name="myInput"
+            defaultValue="Example Task"
+            onChange={(e) => setTask(e.target.value)}
+          />
+        </label>
+        <button className="button1" onClick={() => handleClick()}>
+          Add
+        </button>
         {taskList.map((task, index) => (
           <div key={index}>
-            <input 
-              type="checkbox" 
-              onChange={() => handleTaskCompletion(index)} 
+            <input
+              type="checkbox"
+              checked={completedTasks[index]}
+              onChange={() => handleTaskCompletion(index)}
             />
-            {task}
+            <span
+              style={
+                completedTasks[index] ? { textDecoration: "line-through" } : {}
+              }
+            >
+              {task}
+            </span>
           </div>
         ))}
-        </div>
+        <button className="button2" onClick={() => clearAllTasks()}>
+          Clear All Tasks
+        </button>
+      </div>
     </div>
   );
 }
